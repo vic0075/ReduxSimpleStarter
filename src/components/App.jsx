01201a5aa2372ec28f390aa3294a0cf403bf056b
vikash-tiwari <vikash.tiwari@tothenew.com>
2018-05-23 19:18:03 +0530

    add all component skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return (
+        <div>
+            <h2>React simple starter</h2>
+            <div>
+                <SearchBar/>
+            </div>
+        </div>
+    );
+};
+
+export default App;
