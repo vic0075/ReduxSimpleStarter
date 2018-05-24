691f067473ac7b39dd6b7f71dd8266877f7b0b23
vikash-tiwari <vikash.tiwari@tothenew.com>
2018-05-24 14:08:40 +0530

    chaptor 15

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default class SearchBar extends React.Component {
     render() {
-        return <input type='text'/>;
+        return <input onChange={this.onInputChange}/>;
     }
+
+    // onInputChange(event) {
+    //     // console.log(event.target.value);
+    //     console.log(event);
+    //     // setTimeout(() => {
+    //     //     console.log(event.target.value);
+    //     // }, 800);
+    // }
 };
